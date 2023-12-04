dc2209cc4edd427d497bb51710eed2cc68709218
giacomotolari <tolarigiacomo@gmail.com>
2023-12-04 14:11:06 +0100

    ind other solution to import a local image

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,14 @@ function App() {
         <p>age: {person.age}</p>
         <p>street: {person.address.street}</p>
         <p>city: {person.address.city}</p>
+        {/* image1 was saved locally and imported, image 2 is taken from the web normally as in html */}
         <img
           src="https://images.unsplash.com/photo-1682687981630-cefe9cd73072?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="sea diving"
         />
-        {/* if the image is saved locally, we need to import it and use the import name (image imported) as "src value" */}
-        <img src={seaDiving} alt="sea diving" />
+        {/* if the image is saved locally, we need to import it and use the import name (image imported) as "src value" or start the path from src like in the example below*/}
+        {/* <img src="src/assets/sea-diving.avif" alt="sea diving" /> */}
+        <img src={seaDiving} />
       </div>
     </>
   );
