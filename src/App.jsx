d87f62c4086b3d0e758ee728e04ca5a3e18640dd
giacomotolari <tolarigiacomo@gmail.com>
2023-12-04 13:58:37 +0100

    image1 was saved locally and imported, image 2 is taken from the web normally as in html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import seaDiving from "./assets/sea-diving.avif";
 
 function App() {
   const person = {
@@ -19,6 +20,12 @@ function App() {
         <p>age: {person.age}</p>
         <p>street: {person.address.street}</p>
         <p>city: {person.address.city}</p>
+        <img
+          src="https://images.unsplash.com/photo-1682687981630-cefe9cd73072?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="sea diving"
+        />
+        {/* if the image is saved locally, we need to import it and use the import name (image imported) as "src value" */}
+        <img src={seaDiving} alt="sea diving" />
       </div>
     </>
   );
